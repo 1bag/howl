e784cd0bcdd093fdcf186db450e4573a4e908db6
1bag <kdaproblem@mail.com>
2024-11-01 01:39:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,13 +48,36 @@ ADMIN_CHANNEL_ID = 1301541083687157866  # The admin channel ID to send updates
 # Add the authorized user IDs here
 AUTHORIZED_USERS = {1095069376983613623, 1164595038064496863, 1033671521283084358, 1088497016147025951}
 
+# Set default command prefix
+DEFAULT_PREFIX = "!"
+prefixes = {}
+
+# Load the prefix from a JSON file
+def load_prefix():
+    if os.path.exists('prefix.json'):
+        with open('prefix.json', 'r') as f:
+            return json.load(f)
+    return {}
+
+# Save the prefix to a JSON file
+def save_prefix(prefix_data):
+    with open('prefix.json', 'w') as f:
+        json.dump(prefix_data, f, indent=4)
+
+# Load the prefixes at the start
+prefixes = load_prefix()
+
 # Setting up intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
-intents.members = True  
+intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Create a command prefix callable
+def get_prefix(bot, message):
+    return prefixes.get(str(message.guild.id), DEFAULT_PREFIX)
+
+bot = commands.Bot(command_prefix=get_prefix, intents=intents)
 
 # Load the boost history from a JSON file
 def load_boost_history():
@@ -81,7 +104,7 @@ async def on_member_join(member):
     embed = discord.Embed(
         title="Member Joined",
         description=f"{member.mention} has joined the server!",
-        color=0x00ff00
+        color=0xff8500  # Set embed color to 0xff8500
     )
     admin_channel = bot.get_channel(ADMIN_CHANNEL_ID)
     if admin_channel:
@@ -92,7 +115,7 @@ async def on_member_remove(member):
     embed = discord.Embed(
         title="Member Left",
         description=f"{member.mention} has left the server.",
-        color=0xff0000
+        color=0xff8500  # Set embed color to 0xff8500
     )
     admin_channel = bot.get_channel(ADMIN_CHANNEL_ID)
     if admin_channel:
@@ -108,7 +131,7 @@ async def on_member_update(before, after):
         embed = discord.Embed(
             title="Boosted Server",
             description=f"{after.mention} has boosted the server!",
-            color=0x00ff00
+            color=0xff8500  # Set embed color to 0xff8500
         )
         admin_channel = bot.get_channel(ADMIN_CHANNEL_ID)
         if admin_channel:
@@ -123,7 +146,7 @@ async def on_member_update(before, after):
             embed = discord.Embed(
                 title="Boost Expired",
                 description=f"{before.mention} has unboosted the server.",
-                color=0xff0000
+                color=0xff8500  # Set embed color to 0xff8500
             )
             admin_channel = bot.get_channel(ADMIN_CHANNEL_ID)
             if admin_channel:
@@ -145,6 +168,9 @@ async def update_boost_history():
 
 @bot.command()
 async def tos(ctx):
+    """Usage: !tos
+    Shows the Terms of Service and rules of the server.
+    Only accessible by authorized users."""
     if ctx.author.id not in AUTHORIZED_USERS:
         return await ctx.send("You are not authorized to use this command.")
 
@@ -163,12 +189,15 @@ async def tos(ctx):
             "**Spamming DMs:** Not allowed (Permanent Ban)\n\n"
             "**Discord TOS:** Follow Discord's TOS and Guidelines: https://discord.com/terms | https://discord.com/guidelines"
         ),
-        color=0xff8500
+        color=0xff8500  # Set embed color to 0xff8500
     )
     await ctx.send(embed=embed)
 
 @bot.command()
 async def boost(ctx):
+    """Usage: !boost
+    Displays the list of users who have boosted the server.
+    Can be used in any server."""
     if ctx.guild is None:
         return await ctx.send("This command can only be used in a server.")
 
@@ -179,32 +208,209 @@ async def boost(ctx):
         embed = discord.Embed(
             title="Boost List",
             description=f"The following users have boosted the server:\n{boost_description}",
-            color=0xff8500
+            color=0xff8500  # Set embed color to 0xff8500
         )
     else:
         embed = discord.Embed(
             title="Boost List",
             description="No users have boosted the server.",
-            color=0xff8500
+            color=0xff8500  # Set embed color to 0xff8500
+        )
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def whitelist_list(ctx):
+    """Usage: !whitelist_list
+    Lists all members with the whitelist role.
+    Can be used by anyone."""
+    role_id = 1301549273959104594  # Role ID to check
+    role = ctx.guild.get_role(role_id)
+
+    if role is None:
+        return await ctx.send("The specified role could not be found.")
+
+    members_with_role = role.members  # Get members who have this role
+    if members_with_role:
+        member_list = [f"<@{member.id}> - {member.display_name}" for member in members_with_role]
+        member_list_str = "\n".join(member_list)
+
+        embed = discord.Embed(
+            title=f"Members with the whitelist role: {role.name}",
+            description=member_list_str,
+            color=0xff8500  # Set embed color to 0xff8500
+        )
+    else:
+        embed = discord.Embed(
+            title=f"Members with the whitelist role: {role.name}",
+            description="No members have this role.",
+            color=0xff8500  # Set embed color to 0xff8500
         )
 
     await ctx.send(embed=embed)
 
 @bot.command()
 async def whitelist(ctx, member: discord.Member):
+    """Usage: !whitelist @user
+    Adds a user to the whitelist role.
+    Restricted to authorized users only."""
     if ctx.author.id not in AUTHORIZED_USERS:
         return await ctx.send("You are not authorized to use this command.")
 
-    role_id = 1301549270973026345  # Update this role ID to the one you wish to assign
+    role_id = 1301549273959104594  # Role ID to add
     role = ctx.guild.get_role(role_id)
 
     if role is None:
         return await ctx.send("The specified role could not be found.")
 
-    await member.add_roles(role)
+    try:
+        await member.add_roles(role)
+        await ctx.send(f"{member.mention} has been added to the whitelist role {role.name}.")
+    except discord.Forbidden:
+        await ctx.send("I do not have permissions to add roles.")
+    except discord.HTTPException:
+        await ctx.send("Failed to add role. Please try again.")
+
+@bot.command()
+async def blacklist(ctx, member: discord.Member):
+    """Usage: !blacklist @user
+    Removes a user from the whitelist role.
+    Restricted to authorized users only."""
+    if ctx.author.id not in AUTHORIZED_USERS:
+        return await ctx.send("You are not authorized to use this command.")
+
+    role_id = 1301549273959104594  # Role ID to remove from the user
+    role = ctx.guild.get_role(role_id)
+
+    if role is None:
+        return await ctx.send("The specified role could not be found.")
+
+    try:
+        await member.remove_roles(role)
+        await ctx.send(f"{member.mention} has been removed from the whitelist role {role.name}.")
+    except discord.Forbidden:
+        await ctx.send("I do not have permissions to remove roles.")
+    except discord.HTTPException:
+        await ctx.send("Failed to remove role. Please try again.")
+
+@bot.command()
+async def prices(ctx):
+    """Usage: !prices
+    Shows the pricing for Howl scripts and associated items.
+    Can be used by anyone."""
     embed = discord.Embed(
-        title="Whitelisting Successful",
-        description=f"<@{member.id}> has been whitelisted!",
+        title="**Howl Script Pricing**",
+        description=(
+            "Newcomer `->` $20\n"
+            "HowlV2 Buyer `->` $15\n"
+            "Robux `->` 1,700\n"
+            "HowlV2 Buyer Robux `->` 1,400\n"
+            "Create a ticket here `->` https://discord.com/channels/1283571069491609610/1301549921941458945"
+        ),
+        color=0xff8500  # Set embed color to 0xff8500
+    )
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def info(ctx, command_name: str):
+    """Usage: !info <command_name>
+    Provides information about a specific command, including its usage and functionality.
+    Can be used by anyone."""
+    command_info = {
+        "tos": "The `!tos` command shows the Terms of Service and rules of the server.",
+        "boost": "The `!boost` command displays the list of users who have boosted the server.",
+        "roles": "The `!roles` command lists all the roles available in the server.",
+        "whitelist_list": "The `!whitelist_list` command lists members with the whitelist role.",
+        "whitelist": "The `!whitelist @user` command adds a user to the whitelist role.",
+        "blacklist": "The `!blacklist @user` command removes a user from the whitelist role.",
+        "prices": "The `!prices` command shows the pricing for Howl scripts and associated items.",
+        "ping": "The `!ping` command returns the bot's current latency.",
+        "userinfo": "The `!userinfo @member` command displays information about the specified user.",
+        "serverinfo": "The `!serverinfo` command displays information about the server.",
+        "prefix": "The `!prefix <new_prefix>` command allows you to change the command prefix for the bot."
+    }
+
+    command_name = command_name.lower()
+    info_message = command_info.get(command_name, "Command not found.")
+
+    embed = discord.Embed(
+        title=f"Information about `{command_name}`",
+        description=info_message,
+        color=0xff8500  # Set embed color to 0xff8500
+    )
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def ping(ctx):
+    """Usage: !ping
+    Returns the bot's current latency in milliseconds.
+    Can be used by anyone."""
+    latency = round(bot.latency * 1000)  # Convert to milliseconds
+    embed = discord.Embed(
+        title="Pong",
+        description=f"Latency: {latency} ms",
+        color=0xff8500  # Set embed color to 0xff8500
+    )
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def userinfo(ctx, member: discord.Member = None):
+    """Usage: !userinfo @member
+    Displays information about the specified user or the command author if no user is mentioned.
+    Can be used by anyone."""
+    if member is None:
+        member = ctx.author  # If no member is specified, default to the command author
+
+    roles = [role.mention for role in member.roles if role.name != "@everyone"]  # Exclude the default @everyone role
+    embed = discord.Embed(
+        title=f"User Info for {member.display_name}",
+        color=0xff8500  # Set embed color to 0xff8500
+    )
+    embed.add_field(name="User ID", value=member.id, inline=False)
+    embed.add_field(name="Joined Server On", value=member.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+    embed.add_field(name="Account Created On", value=member.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+    embed.add_field(name="Roles", value=", ".join(roles) if roles else "No roles", inline=False)
+    embed.add_field(name="Status", value=str(member.status).title(), inline=False)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def serverinfo(ctx):
+    """Usage: !serverinfo
+    Displays information about the server, including ID, owner, and member count.
+    Can be used by anyone."""
+    guild = ctx.guild  # The guild where the command was invoked
+    embed = discord.Embed(
+        title=f"Server Info for {guild.name}",
+        color=0xff8500  # Set embed color to 0xff8500
+    )
+    embed.add_field(name="Server ID", value=guild.id, inline=False)
+    embed.add_field(name="Owner", value=guild.owner.mention, inline=False)
+    embed.add_field(name="Created On", value=guild.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+    embed.add_field(name="Member Count", value=guild.member_count, inline=False)
+    embed.add_field(name="Role Count", value=len(guild.roles), inline=False)
+
+    # Correctly fetching the region
+    region = str(guild.region) if hasattr(guild, 'region') else "N/A"
+    embed.add_field(name="Region", value=region, inline=False)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def prefix(ctx, new_prefix: str):
+    """Usage: !prefix <new_prefix>
+    Changes the command prefix for the bot.
+    Restricted to authorized users only."""
+    if ctx.author.id not in AUTHORIZED_USERS:
+        return await ctx.send("You are not authorized to use this command.")
+
+    # Update the prefix in the prefixes dictionary
+    prefixes[str(ctx.guild.id)] = new_prefix
+    save_prefix(prefixes)
+
+    embed = discord.Embed(
+        title="Prefix Change",
+        description=f"The command prefix has been changed to `{new_prefix}`.",
         color=0xff8500
     )
     await ctx.send(embed=embed)
